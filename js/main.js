9cc5815bfbb4ea914481c1be256609c20df25fbb
Samandar-Umidov <umidovsamandar7@gmail.com>
2023-08-28 18:14:53 +0500

    Add search input

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
-var elList = document.querySelector(".pokemon__list");
-var elInput = document.querySelector(".input");
-
-var renderPokemon = function(array) {
+const elList = document.querySelector(".pokemon__list");
+const elInput = document.querySelector(".input");
+const elmodalTitle = document.querySelector(".modal-title");
+const modalBody = document.querySelector("modal-body")
+const renderPokemon = function(array) {
     elList.innerHTML = ""
     array.forEach(element => {
+        var btnElement = document.createElement("button")
         var liElement = document.createElement("li");
         var imgElement = document.createElement("img");
         var weightElement = document.createElement("p");
@@ -11,6 +13,7 @@ var renderPokemon = function(array) {
         var eggElement = document.createElement("p");
         var spawnElement = document.createElement("p");
         var nameElement = document.createElement("p");
+        const  moreBtnElement = document.createElement("button");
         
         liElement.classList.add("pokemon__items");
         
@@ -19,37 +22,60 @@ var renderPokemon = function(array) {
         imgElement.setAttribute("alt", element.name);
         imgElement.classList.add("pokemon-img");
         
+
+        // btnElement.textContent = `Read more`;
+        // btnElement.classList.add("button");
+        // weightElement.textContent = `Weight: ${element.weight}`;
+        // weightElement.classList.add("item__weight");
         
-        weightElement.textContent = `Weight: ${element.weight}`;
-        weightElement.classList.add("item__weight");
+        // heightElement.textContent = `Height: ${element.height}`;
+        // heightElement.classList.add("item__height");
         
-        heightElement.textContent = `Height: ${element.height}`;
-        heightElement.classList.add("item__height");
+        // eggElement.textContent = `Egg: ${element.egg}`;
+        // eggElement.classList.add("item__egg");
         
-        eggElement.textContent = `Egg: ${element.egg}`;
-        eggElement.classList.add("item__egg");
-        
-        spawnElement.textContent = `Spawn time: ${element.spawn_time}`;
-        spawnElement.classList.add("item__spawn");
-        
+        // spawnElement.textContent = `Spawn time: ${element.spawn_time}`;
+        // spawnElement.classList.add("item__spawn");
+                
+        moreBtnElement.textContent = `more`;
+        moreBtnElement.classList.add("more-btn", 'btn-primary', "modal-btn");
+        moreBtnElement.setAttribute('data-bs-toggle', 'modal');
+        moreBtnElement.setAttribute('data-bs-target', '#exampleModal');
+        moreBtnElement.dataset.id = element.id;
+
+
         nameElement.textContent = element.name;
         nameElement.classList.add("item__name");
-        
-        liElement.appendChild(imgElement);
+
+
+        liElement.appendChild(imgElement); 
         liElement.appendChild(weightElement);
-        liElement.appendChild(heightElement);
+        liElement.appendChild(heightElement);   
         liElement.appendChild(eggElement);
         liElement.appendChild(spawnElement);
+        liElement.append(moreBtnElement);
         liElement.appendChild(nameElement);
         elList.appendChild(liElement);
+        
     });
+
 };
 
 renderPokemon(pokemons)
 
+elList.addEventListener('click', (evt) => {
+  if (evt.target.matches('.modal-btn')) {
+    const btnId = evt.target.dataset.id;
+    const finded = pokemons.find(function(item) {
+        return pokemons.id  == btnId;
+    }) 
+    console.log(finded);
+  }
+})
+
 elInput.addEventListener("keyup", function(){
     
-    var inputValue = elInput.value.trim().toLowerCase();
+    const inputValue = elInput.value.trim().toLowerCase();
     
     let result = pokemons.filter(function(item){
         let search_name = item.name.toLowerCase();
@@ -58,6 +84,4 @@ elInput.addEventListener("keyup", function(){
     })
     renderPokemon(result);
 })
-
-
 
